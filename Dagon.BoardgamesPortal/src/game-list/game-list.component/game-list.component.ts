e31dcfc6e39eb115371264b5bdd929207b8456d1
Anton Ilin <anton_ilyin2@epam.com>
2018-01-26 18:28:02 +0200

    Games list component

---
+++ Dagon.BoardgamesPortal/src/game-list/game-list.component/game-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core/src/metadata/directives';
+
+@Component({
+  selector: 'game-list',
+  templateUrl: './game-list.component.html'
+})
+export class GameListComponent {
+
+}
---
+++ Dagon.BoardgamesPortal/src/game-list/game-list.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core/src/metadata/ng_module';
+import { GameListComponent } from './game-list.component/game-list.component';
+
+@NgModule({
+  declarations: [
+    GameListComponent
+  ]
+})
+export class GamelistComponent {
+
+}
